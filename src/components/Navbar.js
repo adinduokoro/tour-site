22c849dcac6fc69385b747ae12d3735a0da5fdde
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2022-12-09 14:44:20 -0500

    Finished creating the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,28 @@
 import './NavbarStyles.css'
 import { Link } from 'react-router-dom'
 import { MenuItems } from './MenuItems'
+import { Component } from 'react'
+import { useState } from 'react'
 
 
-function Navbar() {
+function Navbar () {
+  const [click, setClick] = useState(false)
+
+  const handleClick = () => {
+    setClick(!click)
+  }
+
   return (
     <nav className='NavbarItems'>
-      <h1 className='navbar-logo'>Trippy</h1>
-      <ul className='nav-menu'>
+      <h1 className='navbar-logo'>
+        Trippy
+      </h1>
+
+      <div className='menu-icons'onClick={handleClick}>
+        <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
+      </div>
+
+      <ul className={click ? 'nav-menu active' : "nav-menu"}>
         {MenuItems.map((item, index) => {
           return(
             <li key={index}>
